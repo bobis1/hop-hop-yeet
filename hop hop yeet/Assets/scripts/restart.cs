6e3a7768d64f7f78ba1f6765627410bcfee58b67
bobis <josephwaizy@gmail.com>
2025-08-29 18:11:23 -0500

    added basic level

---
+++ hop hop yeet/Assets/scripts/restart.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour
+{
+    public Button yourButton;
+    public int currentLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(Reset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   public void Reset()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+}
